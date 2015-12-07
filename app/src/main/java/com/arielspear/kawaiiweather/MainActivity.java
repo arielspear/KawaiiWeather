9a25ece96f12e572a1db0077fffec1319dfb7d67
Ariel Spear <arielspear@github.com>
2015-12-07 11:26:02 -0800

    check network availibility.

--- app/src/main/java/com/arielspear/kawaiiweather/MainActivity.java
+++ app/src/main/java/com/arielspear/kawaiiweather/MainActivity.java
@@ -1,11 +1,15 @@
 package com.arielspear.kawaiiweather;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -30,34 +34,50 @@ public class MainActivity extends AppCompatActivity {
         String forecastURL = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecastURL)
-                .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+        if (isNetworkAvailible()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-
-                    Log.v(TAG, response.body().string());
-                    if (response.isSuccessful()) {
-                    }
-                    else{
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught: ", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, R.string.network_unavailible_message, Toast.LENGTH_LONG).show();
+        }
         Log.d(TAG, "Main UI code is running");
+    }
+
+    private boolean isNetworkAvailible() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailible = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailible = true;
+        }
+        return isAvailible;
     }
 
     private void alertUserAboutError() {
